464ffdd69ead67fc41eaf34cdc701efcddd41259
lrb-user <ngy1229@naver.com>
2024-06-11 23:42:46 +0900

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import MobileLoginLayout from './components/Layout/MobileLoginLayout';
 import MobileLayout from './components/Layout/MobileLayout';
 import MobileMain from './components/Pages/Mobile/MobileMain';
 import MobileMovieContentPage from './components/Pages/Mobile/MobileMovieContentPage';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import LoginPage from './components/Pages/LoginPage';
 import Main from './components/Pages/Main';
@@ -16,34 +15,38 @@ import LoginLayout from './components/Layout/LoginLayout';
 function App() {
 
   return (
-    <>
-      <BrowserView>
-        <Router>
-                <Routes>
-                    <Route element={<LoginLayout />}>
-                        <Route path="/" element={<LoginPage />}/>
-                    </Route>
-                    <Route element={<Layout/>}>
-                        <Route path="/main" element={<Main />}/>
-                        <Route path="/movieContentPage" element={<MovieContentPage />}/>
-                    </Route>
-                </Routes>
-            </Router>
-      </BrowserView>
-      <MobileView>
-        <Router>
-                <Routes>
-                    <Route element={<MobileLoginLayout />}>
-                        <Route path="/" element={<MobileLoginPage />}/>
-                    </Route>
-                    <Route element={<MobileLayout />}>
-                        <Route path="/mobileMain" element={<MobileMain />}/>
-                        <Route path="/mobileMovieContentPage" element={<MobileMovieContentPage />}/>
-                    </Route>
-                </Routes>
-            </Router>
-      </MobileView>
-    </>
+    <Router basename={process.env.PUBLIC_URL}>
+
+      <>
+
+        <BrowserView>
+          <Router>
+                  <Routes>
+                      <Route element={<LoginLayout />}>
+                          <Route path="/" element={<LoginPage />}/>
+                      </Route>
+                      <Route element={<Layout/>}>
+                          <Route path="/main" element={<Main />}/>
+                          <Route path="/movieContentPage" element={<MovieContentPage />}/>
+                      </Route>
+                  </Routes>
+              </Router>
+        </BrowserView>
+        <MobileView>
+          <Router>
+                  <Routes>
+                      <Route element={<MobileLoginLayout />}>
+                          <Route path="/" element={<MobileLoginPage />}/>
+                      </Route>
+                      <Route element={<MobileLayout />}>
+                          <Route path="/mobileMain" element={<MobileMain />}/>
+                          <Route path="/mobileMovieContentPage" element={<MobileMovieContentPage />}/>
+                      </Route>
+                  </Routes>
+              </Router>
+        </MobileView>
+      </>
+    </Router>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom'
+// import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    // <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     
-    </BrowserRouter>
+    // </HashRouter>
   // </React.StrictMode>,
 )
